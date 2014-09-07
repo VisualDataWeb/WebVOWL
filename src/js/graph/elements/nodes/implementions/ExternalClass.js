647f863a81177a57325cd3c6cdd36787109f7f38
linkvt <mail@linkvt.de>
2014-09-07 21:54:54 +0000

    Fix representation of External Classes

--- src/js/graph/elements/nodes/implementions/ExternalClass.js
+++ src/js/graph/elements/nodes/implementions/ExternalClass.js
@@ -3,7 +3,8 @@ webvowl.nodes.externalclass = (function () {
 	var o = function () {
 		webvowl.nodes.RoundNode.call(this);
 
-		this.styleClass("external")
+		this.indication("external")
+			.styleClass("external")
 			.textAttribute("white")
 			.type("ExternalClass");
 	};
