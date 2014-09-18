d81c6c8db667c9e1d1b18e173b2b682c17c0b022
linkvt <mail@linkvt.de>
2014-09-18 23:11:04 +0000

    Highlight links when hovering set operator nodes

--- src/js/graph/elements/nodes/SetOperatorNode.js
+++ src/js/graph/elements/nodes/SetOperatorNode.js
@@ -5,7 +5,16 @@ webvowl.nodes.SetOperatorNode = (function () {
 	var o = function () {
 		webvowl.nodes.RoundNode.call(this);
 
+		var that = this,
+			superHoverHighlightingFunction = this.setHoverHighlighting;
+
 		this.radius(radius);
+
+		this.setHoverHighlighting = function (enable) {
+			superHoverHighlightingFunction(enable);
+
+			d3.selectAll("." + that.cssClassOfNode()).classed("hovered", enable);
+		};
 	};
 	o.prototype = Object.create(webvowl.nodes.RoundNode.prototype);
 	o.prototype.constructor = o;
