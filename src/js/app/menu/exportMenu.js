1f3cb68a06cc2620dc0067598e747a89f5751887
linkvt <mail@linkvt.de>
2014-09-21 17:54:58 +0000

    Reorder vowl.css partitionally to fix export

--- src/js/app/menu/exportMenu.js
+++ src/js/app/menu/exportMenu.js
@@ -49,26 +49,26 @@ webvowlApp.exportMenu = function (graphSelector, ontologyName) {
 		d3.selectAll(".subtext").style("font-size", "9px");
 		d3.selectAll(".cardinality").style("font-size", "10px");
 		d3.selectAll(".text, .embedded").style("pointer-events", "none");
-		d3.selectAll(".class, path, line").style("stroke-width", "2");
-		d3.selectAll(".fineline").style("stroke-width", "1");
-		d3.selectAll(".special").style("stroke-dasharray", "8");
-		d3.selectAll(".dotted").style("stroke-dasharray", "3");
-		d3.selectAll("rect.focused, circle.focused").style("stroke-width", "4px");
-		d3.selectAll(".nostroke").style("stroke", "none");
 		d3.selectAll(".class, .object, .disjoint, .objectproperty, .disjointwith, .equivalentproperty, .transitiveproperty, .functionalproperty, .inversefunctionalproperty, .symmetricproperty").style("fill", "#acf");
 		d3.selectAll(".label .datatype, .datatypeproperty").style("fill", "#9c6");
 		d3.selectAll(".rdf, .rdfproperty").style("fill", "#c9c");
 		d3.selectAll(".literal, .node .datatype").style("fill", "#fc3");
 		d3.selectAll(".deprecated, .deprecatedproperty").style("fill", "#ccc");
 		d3.selectAll(".external, .externalproperty").style("fill", "#36c");
-		d3.selectAll(".symbol").style("fill", "#69c");
 		d3.selectAll("path, .nofill").style("fill", "none");
+		d3.selectAll(".symbol").style("fill", "#69c");
 		d3.selectAll(".arrowhead, marker path").style("fill", "#000");
 		d3.selectAll(".class, path, line, .fineline").style("stroke", "#000");
 		d3.selectAll(".white, .subclass, .dottedMarker path, .subclassproperty, .external + text").style("fill", "#fff");
 		d3.selectAll(".class.hovered, .property.hovered, path.arrowhead.hovered, .cardinality.hovered, .normalMarker path.hovered, .cardinality.focused, .normalMarker path.focused, circle.pin").style("fill", "#f00").style("cursor", "pointer");
 		d3.selectAll(".focused, path.hovered").style("stroke", "#f00");
 		d3.selectAll(".label .indirectHighlighting, .feature:hover").style("fill", "#f90");
+		d3.selectAll(".class, path, line").style("stroke-width", "2");
+		d3.selectAll(".fineline").style("stroke-width", "1");
+		d3.selectAll(".special").style("stroke-dasharray", "8");
+		d3.selectAll(".dotted").style("stroke-dasharray", "3");
+		d3.selectAll("rect.focused, circle.focused").style("stroke-width", "4px");
+		d3.selectAll(".nostroke").style("stroke", "none");
 		d3.selectAll("#width-test").style("position", "absolute").style("float", "left").style("white-space", "nowrap").style("visibility", "hidden");
 		d3.selectAll("marker path").style("stroke-dasharray", "50");
 	}
@@ -81,7 +81,7 @@ webvowlApp.exportMenu = function (graphSelector, ontologyName) {
 	}
 
 	function removeVowlInlineStyles() {
-		d3.selectAll(".text, .subtext, .cardinality, .text, .embedded, .class, path, line, .fineline, .special, .dotted, rect.focused, circle.focused, .nostroke, .class, .object, .disjoint, .objectproperty, .disjointwith, .equivalentproperty, .transitiveproperty, .functionalproperty, .inversefunctionalproperty, .symmetricproperty, .label .datatype, .datatypeproperty, .rdf, .rdfproperty, .literal, .node .datatype, .deprecated, .deprecatedproperty, .external, .externalproperty, .symbol, path, .nofill, .arrowhead, marker path, .class, path, line, .fineline, .white, .subclass, .dottedMarker path, .subclassproperty, .external + text, .class.hovered, .property.hovered, path.arrowhead.hovered, .cardinality.hovered, .normalMarker path.hovered, .cardinality.focused, .normalMarker path.focused, circle.pin, .focused, path.hovered, .label .indirectHighlighting, .feature:hover, #width-test, marker path").attr("style", null);
+		d3.selectAll(".text, .subtext, .cardinality, .text, .embedded, .class, .object, .disjoint, .objectproperty, .disjointwith, .equivalentproperty, .transitiveproperty, .functionalproperty, .inversefunctionalproperty, .symmetricproperty, .label .datatype, .datatypeproperty, .rdf, .rdfproperty, .literal, .node .datatype, .deprecated, .deprecatedproperty, .external, .externalproperty, path, .nofill, .symbol, .arrowhead, marker path, .class, path, line, .fineline, .white, .subclass, .dottedMarker path, .subclassproperty, .external + text, .class.hovered, .property.hovered, path.arrowhead.hovered, .cardinality.hovered, .normalMarker path.hovered, .cardinality.focused, .normalMarker path.focused, circle.pin, .focused, path.hovered, .label .indirectHighlighting, .feature:hover, .class, path, line, .fineline, .special, .dotted, rect.focused, circle.focused, .nostroke, #width-test, marker path").attr("style", null);
 	}
 
 	function showNotExportableElements() {
