f1ba74e1e2f0d0bb8e06853f08e83c4cad9b88aa
linkvt <mail@linkvt.de>
2014-11-26 11:55:44 +0000

    Resize set operators correctly

--- src/js/graph/elements/nodes/implementions/owlintersectionOf.js
+++ src/js/graph/elements/nodes/implementions/owlintersectionOf.js
@@ -26,18 +26,20 @@ webvowl.nodes.owlintersectionof = (function () {
 			symbol.append("circle")
 				.attr("class", "nofill")
 				.classed("fineline", true)
-				.attr("r", (that.actualRadius() - 15));
+				.attr("r", (that.radius() - 15));
 			symbol.append("circle")
 				.attr("cx", 10)
 				.attr("class", "nofill")
 				.classed("fineline", true)
-				.attr("r", (that.actualRadius() - 15));
+				.attr("r", (that.radius() - 15));
 			symbol.append("path")
 				.attr("class", "nofill")
 				.attr("d", "m 9,5 c 0,-2 0,-4 0,-6 0,0 0,0 0,0 0,0 0,-1.8 -1,-2.3 -0.7,-0.6 -1.7,-0.8 -2.9," +
 					"-0.8 -1.2,0 -2,0 -3,0.8 -0.7,0.5 -1,1.4 -1,2.3 0,2 0,4 0,6");
-			symbol.attr("transform", "translate(-" + (that.actualRadius() - 15) / 5 + ",-" +
-				(that.actualRadius() - 15) / 100 + ")");
+
+			var scale = that.actualRadius() / that.radius();
+			symbol.attr("transform", "translate(-" + scale * (that.radius() - 15) / 5 + ",-" +
+				(that.radius() - 15) / 100 + ")scale(" + scale + ")");
 
 			that.postDrawActions();
 		};
