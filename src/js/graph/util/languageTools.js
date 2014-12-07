b27b9e30ff1fc1e230959ed8e5b4b7906d968db6
linkvt <mail@linkvt.de>
2014-12-07 19:44:58 +0000

    Update multi language selection order

--- src/js/graph/util/languageTools.js
+++ src/js/graph/util/languageTools.js
@@ -15,15 +15,30 @@ webvowl.util.languageTools = (function () {
 			return textObject[preferredLanguage];
 		}
 
-		for (var language in textObject) {
-			if (textObject.hasOwnProperty(language) && language !== "default") {
-				return textObject[language];
-			}
+		var textForLanguage = searchLanguage(textObject, "en");
+		if (textForLanguage) {
+			return textForLanguage;
+		}
+		textForLanguage = searchLanguage(textObject, "unset");
+		if (textForLanguage) {
+			return textForLanguage;
+		}
+		textForLanguage = searchLanguage(textObject, "iriBased");
+		if (textForLanguage) {
+			return textForLanguage;
 		}
 
 		return textObject["default"];
 	};
 
+
+	function searchLanguage (textObject, preferredLanguage) {
+		for (var language in textObject) {
+			if (language === preferredLanguage && textObject.hasOwnProperty(language)) {
+				return textObject[language];
+			}
+		}
+	}
 
 	return function () {
 		/* Use a function here to keep a consistent style like webvowl.path.to.module()
