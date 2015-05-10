VisualDataWeb/WebVOWL

commit 4dd02fb217d3685af774ef4331c6af9c7ad967ab

Tidy grunt file a bit

Gruntfile.js
@@ -74,12 +74,6 @@ module.exports = function (grunt) {
 					{expand: true, cwd: "src/", src: ["favicon.ico"], dest: deployPath},
 					{expand: true, src: ["license.txt"], dest: deployPath}
 				]
-			},
-			deploy: {
-				files: [
-					{expand: true, cwd: "src", src: ".htaccess", dest: deployPath},
-					{expand: true, cwd: "src/log", src: ".dummy", dest: deployPath + "log/"}
-				]
 			}
 		},
 		cssmin: {
@@ -103,15 +97,6 @@ module.exports = function (grunt) {
 				}
 			},
 			dev: {
-				src: "src/index.html",
-				dest: deployPath,
-				options: {
-					data: {
-						version: "<%= pkg.version %>"
-					}
-				}
-			},
-			release: {
 				src: "src/index.html",
 				dest: deployPath
 			}
@@ -175,8 +160,8 @@ module.exports = function (grunt) {
 
 	grunt.registerTask("build-common", ["clean", "copy", "cssmin", "concat", "uglify"]);
 	grunt.registerTask("default", ["release"]);
-	grunt.registerTask("package", ["build-common", "htmlbuild:dev"]);
-	grunt.registerTask("release", ["build-common", "htmlbuild:release"]);
+	grunt.registerTask("package", ["build-common", "htmlbuild"]);
+	grunt.registerTask("release", ["build-common", "htmlbuild"]);
 	grunt.registerTask("webserver", ["package", "connect:devserver", "watch"]);
 	grunt.registerTask("test", ["karma"]);
 };

commit 07e6f98f2493a0df5e7a59e8e9902b18f3896ae9

Fix removing the benchmark ontology in release
The build task was broken since 4dd02fb.

Gruntfile.js
@@ -95,6 +95,11 @@ module.exports = function (grunt) {
 			dev: {
 				src: "src/index.html",
 				dest: deployPath
+			},
+			release: {
+				// required for removing the benchmark ontology from the selection menu
+				src: "src/index.html",
+				dest: deployPath
 			}
 		},
 		jshint: {
@@ -172,8 +177,8 @@ module.exports = function (grunt) {
 
 	grunt.registerTask("build-common", ["clean", "copy", "cssmin", "concat", "uglify", "replace"]);
 	grunt.registerTask("default", ["release"]);
-	grunt.registerTask("package", ["build-common", "htmlbuild"]);
-	grunt.registerTask("release", ["build-common", "htmlbuild"]);
+	grunt.registerTask("package", ["build-common", "htmlbuild:dev"]);
+	grunt.registerTask("release", ["build-common", "htmlbuild:release"]);
 	grunt.registerTask("webserver", ["package", "connect:devserver", "watch"]);
 	grunt.registerTask("test", ["karma"]);
 };