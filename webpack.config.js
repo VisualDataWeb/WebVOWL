9487c72924169984cd84b6105b48303040347365
Vincent Link <mail@linkvt.de>
2015-09-06 15:45:09 +0200

    Change webpack paths

--- webpack.config.js
+++ webpack.config.js
@@ -9,21 +9,21 @@ module.exports = {
 		"webvowl.app": "./src/app/js/entry.js"
 	},
 	output: {
-		path: path.join(__dirname, "deploy/js/"),
-		publicPath: "js/",
-		filename: "[name].js",
-		chunkFilename: "[chunkhash].js",
+		path: path.join(__dirname, "deploy/"),
+		publicPath: "",
+		filename: "js/[name].js",
+		chunkFilename: "js/[chunkhash].js",
 		libraryTarget: "assign",
 		library: "[name]"
 	},
 	module: {
 		loaders: [
 			{test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},
-			{test: /\.json$/, loader: "file"}
+			{test: /\.json$/, loader: "file?name=data/[name].[ext]?[hash]"}
 		]
 	},
 	plugins: [
-		new ExtractTextPlugin("../css/[name].css"),
+		new ExtractTextPlugin("css/[name].css"),
 		new webpack.ProvidePlugin({
 			d3: "d3"
 		})
