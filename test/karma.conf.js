8d800e1dd10e653830ae156f099e4f740f18a9ce
Vincent Link <mail@linkvt.de>
2015-09-06 13:30:28 +0200

    Fix test runners and update dependencies

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -2,30 +2,23 @@ module.exports = function (config) {
 	config.set({
 		basePath: "../",
 		frameworks: ["jasmine"],
-		preprocessors: {
-			"test/unit/**/*.js": ["webpack"]
-		},
 		files: [
 			"node_modules/d3/d3.js",
-			"test/unit/**/*.js"
+			"test/unit/index.js"
 		],
+		preprocessors: {
+			"test/unit/index.js": ["webpack"]
+		},
 		reporters: ["progress"],
 		browsers: ["PhantomJS"],
 		plugins: [
-			"karma-phantomjs-launcher",
-			"karma-jasmine",
-			"karma-webpack"
+			require("karma-phantomjs-launcher"),
+			require("karma-jasmine"),
+			require("karma-webpack")
 		],
 		webpack: {
-			cache: true,
-			entry: {
-				webvowl: "./src/webvowl/js/entry.js"
-			},
-			module: {
-				loaders: [
-					{test: /\.css$/, loader: "style!css"},
-					{test: /\.json$/, loader: "file"}
-				]
+			resolve: {
+				extensions: ["", ".js"]
 			}
 		},
 		webpackMiddleware: {
