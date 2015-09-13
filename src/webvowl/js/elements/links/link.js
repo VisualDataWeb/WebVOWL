05a7329b6680c56fd189c264cf7d2f5a291a2138
Vincent Link <mail@linkvt.de>
2015-09-13 22:14:03 +0200

    Fix duplicate drawing of links

--- src/webvowl/js/elements/links/link.js
+++ src/webvowl/js/elements/links/link.js
@@ -60,11 +60,6 @@ module.exports = function (_domain, _range, _property) {
 		return domain;
 	};
 
-	link.draw = function (linkGroup, markerContainer) {
-		frontPart.draw(linkGroup, markerContainer);
-		backPart.draw(linkGroup, markerContainer);
-	};
-
 	link.frontPart = function () {
 		return frontPart;
 	};
@@ -83,6 +78,69 @@ module.exports = function (_domain, _range, _property) {
 
 	link.range = function () {
 		return range;
+	};
+
+
+	link.draw = function (linkGroup, markerContainer) {
+		var inverse = link.inverse();
+
+		property.linkGroup(linkGroup);
+		if (inverse) {
+			inverse.linkGroup(linkGroup);
+		}
+
+		// Marker for this property
+		property.markerElement(markerContainer.append("marker")
+			.datum(property)
+			.attr("id", property.markerId())
+			.attr("viewBox", "0 -8 14 16")
+			.attr("refX", 12)
+			.attr("refY", 0)
+			.attr("markerWidth", 12)  // ArrowSize
+			.attr("markerHeight", 12)
+			.attr("markerUnits", "userSpaceOnUse")
+			.attr("orient", "auto")  // Orientation of Arrow
+			.attr("class", property.markerType() + "Marker"));
+		property.markerElement()
+			.append("path")
+			.attr("d", "M0,-8L12,0L0,8Z");
+
+		// Marker for the inverse property
+		if (inverse) {
+			inverse.markerElement(markerContainer.append("marker")
+				.datum(inverse)
+				.attr("id", inverse.markerId())
+				.attr("viewBox", "0 -8 14 16")
+				.attr("refX", 0)
+				.attr("refY", 0)
+				.attr("markerWidth", 12)  // ArrowSize
+				.attr("markerHeight", 12)
+				.attr("markerUnits", "userSpaceOnUse")
+				.attr("orient", "auto")  // Orientation of Arrow
+				.attr("class", inverse.markerType() + "Marker"));
+			inverse.markerElement().append("path")
+				.attr("d", "M12,-8L0,0L12,8Z");
+		}
+
+		// Draw the link
+		linkGroup.append("path")
+			.classed("link-path", true)
+			.classed(domain.cssClassOfNode(), true)
+			.classed(range.cssClassOfNode(), true)
+			.classed(property.linkType(), true)
+			.attr("marker-end", function (l) {
+				if (!l.property().isSpecialLink()) {
+					return "url(#" + l.property().markerId() + ")";
+				}
+				return "";
+			})
+			.attr("marker-start", function (l) {
+				var inverse = l.inverse();
+				if (inverse && !inverse.isSpecialLink()) {
+					return "url(#" + inverse.markerId() + ")";
+				}
+				return "";
+			});
 	};
 
 
