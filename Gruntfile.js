3a2ca84390e785a7f4feffb0b7247378055498e6
Vincent Link <mail@linkvt.de>
2015-10-27 18:58:35 +0100

    Don't copy unminified d3 file to deploy directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,7 +63,7 @@ module.exports = function (grunt) {
 		copy: {
 			dependencies: {
 				files: [
-					{expand: true, cwd: "node_modules/d3/", src: ["d3*.js"], dest: deployPath + "/js/"}
+					{expand: true, cwd: "node_modules/d3/", src: ["d3.min.js"], dest: deployPath + "/js/"}
 				]
 			},
 			static: {
