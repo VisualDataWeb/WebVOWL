9d24e6b27a2694af5a9ffc6cd0a62669be26395e
Vincent Link <mail@linkvt.de>
2015-11-06 17:30:05 +0100

    Implement adding empty lines to textElement

--- src/webvowl/js/util/textElement.js
+++ src/webvowl/js/util/textElement.js
@@ -9,10 +9,97 @@ module.exports = function (element) {
 
 	var textElement = {},
 		LINE_DISTANCE = 1,
-		SUBTEXT_CSS_CLASS = "subtext",
 		textBlock = element.append("text")
 			.classed("text", true)
 			.attr("text-anchor", "middle");
+
+	/** Due to browser incompatibilities this has to be hardcoded. This is because Firefox has no
+	 * "offsetHeight" attribute like Chrome to retrieve the absolute pixel height. */
+	var STYLES = {
+		default: {height: 14, cssClass: "text"},
+		subtext: {height: 10, cssClass: "subtext"},
+		instanceCount: {height: 14, cssClass: "instance-count"}
+	};
+
+
+	/**
+	 * Adds a new line of text to the element.
+	 * @param text
+	 */
+	textElement.addText = function (text) {
+		if (text) {
+			addTextline(text);
+		}
+	};
+
+	/**
+	 * Adds a line of text in subproperty style.
+	 * @param text
+	 */
+	textElement.addSubText = function (text) {
+		if (text) {
+			addTextline(text, STYLES.subtext, "(", ")");
+		}
+	};
+
+	/**
+	 * Adds a line of text in equivalent node listing style.
+	 * @param text
+	 */
+	textElement.addEquivalents = function (text) {
+		if (text) {
+			addTextline(text, STYLES.subtext, "[", "]");
+		}
+	};
+
+	/**
+	 * Adds a label with the instance count.
+	 * @param instanceCount
+	 */
+	textElement.addInstanceCount = function (instanceCount) {
+		if (instanceCount) {
+			addTextline(instanceCount.toString(), STYLES.instanceCount);
+		}
+	};
+
+	textElement.addEmptyLine = function (height) {
+		if (height > 0) {
+			textBlock.append("tspan")
+				.datum({height: height})
+				.classed("empty", true)
+				.attr("dy", height + "px")
+				.text(" ");
+			repositionTextBlock();
+		}
+	};
+
+	textElement.setTranslation = function (x, y) {
+		textBlock.attr("transform", "translate(" + x + ", " + y + ")");
+	};
+
+	textElement.clear = function () {
+		textBlock.selectAll("*").remove();
+	};
+
+	function addTextline(text, style, prefix, postfix) {
+		style = style || STYLES.default;
+		var truncatedText = textTools.truncate(text, element.datum().textWidth(), style.cssClass);
+
+		textBlock.append("tspan")
+			.datum(style)
+			.classed(STYLES.default.cssClass, true)
+			.classed(style.cssClass, true)
+			.text(applyPreAndPostFix(truncatedText, prefix, postfix))
+			.attr("x", 0)
+			.attr("dy", function () {
+				var heightInPixels = getPixelHeightOfTextLine(d3.select(this)),
+					siblingCount = getLineCount() - 1,
+					lineDistance = siblingCount > 0 ? LINE_DISTANCE : 0;
+				return heightInPixels + lineDistance + "px";
+			});
+
+		repositionTextBlock();
+	}
 
 	/**
 	 * Repositions the textblock according to its own offsetHeight.
@@ -29,67 +116,12 @@ module.exports = function (element) {
 		textBlock.attr("y", -textBlockHeight * 0.6 + "px");
 	}
 
-	/**
-	 * Adds a new line of text to the element.
-	 * @param text
-	 */
-	textElement.addText = function (text) {
-		addTextline(text);
-	};
-
-	/**
-	 * Adds a line of text in subproperty style.
-	 * @param text
-	 */
-	textElement.addSubText = function (text) {
-		addTextline(text, SUBTEXT_CSS_CLASS, "(", ")");
-	};
-
-	/**
-	 * Adds a line of text in equivalent node listing style.
-	 * @param text
-	 */
-	textElement.addEquivalents = function (text) {
-		addTextline(text, SUBTEXT_CSS_CLASS, "[", "]");
-	};
-
-	/**
-	 * Adds a label with the instance count.
-	 * @param instanceCount
-	 */
-	textElement.addInstanceCount = function (instanceCount) {
-		if (instanceCount) {
-			addTextline(instanceCount.toString(), "instance-count");
-		}
-	};
+	function getPixelHeightOfTextLine(textElement) {
+		return textElement.datum().height;
+	}
 
 	function getLineCount() {
 		return textBlock.property("childElementCount") - textBlock.selectAll(".instance-count").size();
-	}
-
-	function addTextline(text, subtextCssClass, prefix, postfix) {
-		if (!text) {
-			return;
-		}
-
-		var truncatedText, tspan;
-
-		subtextCssClass = subtextCssClass || "text";
-		truncatedText = textTools.truncate(text, element.datum().textWidth(), subtextCssClass);
-
-		tspan = textBlock.append("tspan")
-			.classed("text", true)
-			.classed(subtextCssClass, true)
-			.text(applyPreAndPostFix(truncatedText, prefix, postfix))
-			.attr("x", 0)
-			.attr("dy", function () {
-				var heightInPixels = getPixelHeightOfTextLine(d3.select(this)),
-					siblingCount = getLineCount() - 1,
-					lineDistance = siblingCount > 0 ? LINE_DISTANCE : 0;
-				return heightInPixels + lineDistance + "px";
-			});
-
-		repositionTextBlock();
 	}
 
 	function applyPreAndPostFix(text, prefix, postfix) {
@@ -100,16 +132,6 @@ module.exports = function (element) {
 			text += postfix;
 		}
 		return text;
-	}
-
-	function getPixelHeightOfTextLine(textElement) {
-		/* Due to browser incompatibilities this has to be hardcoded. This is because Firefox has no
-		 * "offsetHeight" attribute like Chrome to retrieve the absolute pixel height. */
-		if (textElement.classed("subtext")) {
-			return 10;
-		} else {
-			return 14;
-		}
 	}
 
 	function getTextBlockHeight(textBlock) {
@@ -129,14 +151,6 @@ module.exports = function (element) {
 
 		return pixelHeight;
 	}
-
-	textElement.setTranslation = function (x, y) {
-		textBlock.attr("transform", "translate(" + x + ", " + y + ")");
-	};
-
-	textElement.clear = function () {
-		textBlock.selectAll("*").remove();
-	};
 
 	return textElement;
 };
