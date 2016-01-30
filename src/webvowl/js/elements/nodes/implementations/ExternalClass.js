41e2ee37dda87bd69480e32f8d7fb5988da62e8b
Vincent Link <mail@linkvt.de>
2016-01-30 17:37:47 +0100

    Don't also set backgroundColor in ExternalClass.js

--- src/webvowl/js/elements/nodes/implementations/ExternalClass.js
+++ src/webvowl/js/elements/nodes/implementations/ExternalClass.js
@@ -5,19 +5,8 @@ module.exports = (function () {
 	var o = function (graph) {
 		RoundNode.apply(this, arguments);
 
-		var that = this;
-		var superDrawFunction = that.draw;
-
 		this.attributes(["external"])
 			.type("ExternalClass");
-
-		this.draw = function (element) {
-			superDrawFunction(element);
-
-			if (this.backgroundColor()) {
-				element.select("circle").style("fill", this.backgroundColor());
-			}
-		};
 	};
 	o.prototype = Object.create(RoundNode.prototype);
 	o.prototype.constructor = o;
