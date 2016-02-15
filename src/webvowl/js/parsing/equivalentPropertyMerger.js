96793c3a8b8d61c99493113f078eae794f478a09
Vincent Link <mail@linkvt.de>
2016-02-15 18:08:27 +0100

    Further refactor the equivalentPropertyMerger

--- src/webvowl/js/parsing/equivalentPropertyMerger.js
+++ src/webvowl/js/parsing/equivalentPropertyMerger.js
@@ -9,34 +9,31 @@ module.exports = function () {
 
 var PREFIX = "GENERATED-MERGED_RANGE-";
 
+
 equivalentPropertyMerger.merge = function (properties, nodes, propertyMap, graph) {
-	var hiddenNodeIds = d3.set();
+	var totalNodeIdsToHide = d3.set();
 	var mergeNodes = [];
 
 	for (var i = 0; i < properties.length; i++) {
 		var property = properties[i];
 		var equivalents = property.equivalents().map(createIdToPropertyMapper(propertyMap));
-		var allEquivalents = equivalents.concat(property);
 
-		if (equivalents.length === 0 || isProcessedProperty(property)) {
+		if (equivalents.length === 0 || isPropertyProcessed(property)) {
 			continue;
 		}
 
-		var mergedRange = createMergeNode(property, graph);
-		mergeNodes.push(mergedRange);
+		var mergeRange = createMergeNode(property, equivalents, graph);
+		mergeNodes.push(mergeRange);
 
-		for (var equivIndex = 0; equivIndex < allEquivalents.length; equivIndex++) {
-			var equivalentProperty = allEquivalents[equivIndex];
+		var propertyWithEquivalents = equivalents.concat(property);
+		var nodeIdsToHide = replaceRangesAndCollectNodesToHide(propertyWithEquivalents, mergeRange, properties);
 
-			var oldRangeId = equivalentProperty.range();
-			equivalentProperty.range(mergedRange.id());
-			if (!isDomainOrRangeOfOtherProperty(oldRangeId, properties)) {
-				hiddenNodeIds.add(oldRangeId);
-			}
+		for (var j = 0; j < nodeIdsToHide; j++) {
+			totalNodeIdsToHide.add(nodeIdsToHide[j]);
 		}
 	}
 
-	return filterVisibleNodes(nodes.concat(mergeNodes), hiddenNodeIds);
+	return filterVisibleNodes(nodes.concat(mergeNodes), totalNodeIdsToHide);
 };
 
 
@@ -46,21 +43,39 @@ function createIdToPropertyMapper(propertyMap) {
 	};
 }
 
-function isProcessedProperty(property) {
+function isPropertyProcessed(property) {
 	return property.range().indexOf(PREFIX) === 0;
 }
 
-function createMergeNode(property, graph) {
+function createMergeNode(property, graph, equivalents) {
 	var range;
 
 	if (elementTools.isDatatypeProperty(property)) {
 		range = new RdfsLiteral(graph);
 	} else {
-		range = new OwlThing(graph);
+		range = findObjectPropertyMergeRange(property, equivalents, new OwlThing(graph));
 	}
 	range.id(PREFIX + property.id());
 
 	return range;
+}
+
+function findObjectPropertyMergeRange(property, equivalents, defaultRange) {
+	return defaultRange;
+}
+
+function replaceRangesAndCollectNodesToHide(propertyWithEquivalents, mergedRange, properties) {
+	var nodesToHide = [];
+
+	propertyWithEquivalents.forEach(function (property) {
+		var oldRangeId = property.range();
+		property.range(mergedRange.id());
+		if (!isDomainOrRangeOfOtherProperty(oldRangeId, properties)) {
+			nodesToHide.push(oldRangeId);
+		}
+	});
+
+	return nodesToHide;
 }
 
 function isDomainOrRangeOfOtherProperty(nodeId, properties) {
@@ -74,11 +89,11 @@ function isDomainOrRangeOfOtherProperty(nodeId, properties) {
 	return false;
 }
 
-function filterVisibleNodes(nodes, hiddenNodeIds) {
+function filterVisibleNodes(nodes, nodeIdsToHide) {
 	var filteredNodes = [];
 
 	nodes.forEach(function (node) {
-		if (!hiddenNodeIds.has(node.id())) {
+		if (!nodeIdsToHide.has(node.id())) {
 			filteredNodes.push(node);
 		}
 
